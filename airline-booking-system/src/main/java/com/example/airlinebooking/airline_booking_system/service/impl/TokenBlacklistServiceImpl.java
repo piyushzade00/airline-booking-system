bd8a64b7a17564553be186dcecb84b5f060c20c4
piyushzade00 <piyushzade00@gmail.com>
2025-02-11 15:48:38 +0530

    Implementing Spring Security and JWT Token Authentication in the project

--- airline-booking-system/src/main/java/com/example/airlinebooking/airline_booking_system/service/impl/TokenBlacklistServiceImpl.java
+++ airline-booking-system/src/main/java/com/example/airlinebooking/airline_booking_system/service/impl/TokenBlacklistServiceImpl.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 @Service
